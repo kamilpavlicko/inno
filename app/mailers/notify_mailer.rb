75022355fdc678b6ecec83a85f71221df081af05
kamil.pavlicko@gmail.com <kamil.pavlicko@gmail.com>
2016-04-24 22:36:05 +0200

    Mailer set

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,8 +1,10 @@
 class NotifyMailer < ApplicationMailer
-default from: "from@example.com
+default from: "from@example.com"
 
 def sample_email(user)
     @user = user
     mail(to: @user.email, subject: 'Critical Notification')
-  end
+end
+
+
 end
